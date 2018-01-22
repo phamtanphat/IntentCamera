2ed5f445074edfe4e6894015a86c2e76acb7cb3a
thuviengithub0208 <khoaphp@yahoo.com>
2018-01-22 18:57:27 +0700

    Initial commit

---
+++ app/src/main/java/com/ptp/phamtanphat/intentcamera/MainActivity.java
@@ -0,0 +1,62 @@
+package com.ptp.phamtanphat.intentcamera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btncamera,btngallery;
+    ImageView img;
+    int Request_Code_Camera = 123;
+    int Request_Code_Gallery = 456;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        anhxa();
+        btncamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,Request_Code_Camera);
+            }
+        });
+        btngallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,Request_Code_Gallery);
+            }
+        });
+    }
+
+    private void anhxa() {
+        btncamera = findViewById(R.id.buttoncamera);
+        btngallery = findViewById(R.id.buttongallery);
+        img = findViewById(R.id.imageview);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Request_Code_Camera && resultCode == RESULT_OK && data != null){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            img.setImageBitmap(bitmap);
+        }
+        if (requestCode == Request_Code_Gallery && resultCode == RESULT_OK && data != null){
+            Uri uri = data.getData();
+            img.setImageURI(uri);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
